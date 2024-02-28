9988e234b249de2db6188da7609d99f35cc0b1a0
kouichi.matsushita <dfhui4ty@yahoo.co.jp>
2024-02-28 23:42:53 +0900

    2024/2/28

--- src/main/java/com/techacademy/controller/EmployeeController.java
+++ src/main/java/com/techacademy/controller/EmployeeController.java
@@ -98,12 +98,58 @@ public class EmployeeController {
         return "redirect:/employees";
     }
 
- // 従業員更新画面
+    // 従業員更新画面
     @GetMapping(value = "/{code}/update")
-    public String edit(@PathVariable String code,@ModelAttribute Employee employee) {
+    public String edit(@PathVariable String code, Model model,Employee employee) {
+        if (null!=code) {
+            model.addAttribute("employee", employeeService.findByCode(code));
+        }else {
+            model.addAttribute("employee", employee);
+        }
 
         return "employees/update";
     }
+ // 従業員更新処理
+    @PostMapping(value = "/{code}/update")
+    public String update(@Validated Employee employee, BindingResult res, Model model) {
+
+        // パスワード空白チェック
+        /*
+         * エンティティ側の入力チェックでも実装は行えるが、更新の方でパスワードが空白でもチェックエラーを出さずに
+         * 更新出来る仕様となっているため上記を考慮した場合に別でエラーメッセージを出す方法が簡単だと判断
+         */
+//        if ("".equals(employee.getPassword())) {
+//            // パスワードが空白だった場合
+//            model.addAttribute(ErrorMessage.getErrorName(ErrorKinds.BLANK_ERROR),
+//                    ErrorMessage.getErrorValue(ErrorKinds.BLANK_ERROR));
+//
+//            return create(employee);
+//
+//        }
+
+        // 入力チェック
+        if (res.hasErrors()) {
+            return edit(null,model,employee);
+        }
+//
+//        // 論理削除を行った従業員番号を指定すると例外となるためtry~catchで対応
+//        // (findByIdでは削除フラグがTRUEのデータが取得出来ないため)
+//        try {
+//            ErrorKinds result = employeeService.save(employee);
+//
+//            if (ErrorMessage.contains(result)) {
+//                model.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
+//                return create(employee);
+//            }
+//
+//        } catch (DataIntegrityViolationException e) {
+//            model.addAttribute(ErrorMessage.getErrorName(ErrorKinds.DUPLICATE_EXCEPTION_ERROR),
+//                    ErrorMessage.getErrorValue(ErrorKinds.DUPLICATE_EXCEPTION_ERROR));
+//            return create(employee);
+//        }
+
+        return "redirect:/employees";
+        }
 
     // 従業員削除処理
     @PostMapping(value = "/{code}/delete")
